b5b53d86470e782d3510ed7fc13f05cd8d577c01
sam-payne <sam_payne99@yahoo.co.uk>
2022-06-23 16:14:38 +0100

    New RandomHadlocks and comments

--- src/testing.py
+++ src/testing.py
@@ -1,17 +1,24 @@
+from distutils.command.config import config
 from gui import drawSB
 from switchbox import Switchbox
 import random
 from routing import *
 from utils import *
+from stats import *
 
-def routeTestSingle(width,routing_method,routeback,nets,number_demands):
+from stats import *
+
+########################
+# Generate a single routing test for a set number of demands
+def routeTestSingle(width,routing_method,routeback,common_nets,number_demands):
 
     demands = []
     for d in range(0,number_demands):
         # Generate the appropriate number of demands
         
-        demands.append(genDemand(width,routeback))
+        demands.append(genDemand(width,routeback,common_nets,demands))
     
+    # Route using given method
     sb = Switchbox(width,demands)
     if routing_method == 'HorizontalFirst':
         HorizontalFirst(sb)
@@ -19,24 +26,29 @@ def routeTestSingle(width,routing_method,routeback,nets,number_demands):
     if routing_method == 'Hadlocks':
         Hadlocks(sb)
     
-    getRouteEfficiency(sb)
-   # drawSB(sb)
+    # Return switch box for further analysis
+    return sb
 
-def routeTestBatch(width,routing_method, routeback, nets, iterations, max_demands):
+###########################
+# Run a number of tests. Run i number of tests (given by 'iterations'), using a set number of demands, increasing up to 'max_demands'
+def routeTestBatch(width,routing_method, routeback, common_nets, max_demands, iterations):
     results = []
     demands = ''
+    startprogress("Running batch test")
+
     for number_demands in range(1,max_demands+1):
         # Repeat up to the maximum number of demands to route
 
         success_counter = 0
 
         for i in range(0,iterations):
+            
             # For a given number of demands, iterate a certain number of times
             demands = []
             for d in range(0,number_demands):
                 # Generate the appropriate number of demands
                 
-                demands.append(genDemand(width,routeback))
+                demands.append(genDemand(width,routeback,common_nets,demands))
             
             sb = Switchbox(width,demands)
             if routing_method == 'HorizontalFirst':
@@ -44,28 +56,42 @@ def routeTestBatch(width,routing_method, routeback, nets, iterations, max_demand
                     HorizontalFirst(sb)
                     #sb.printDemands()
                 except:
+                    print(getAllStats(sb))
                     drawSB(sb)
                     continue
                 else:
+
                     success_counter = success_counter + 1
 
             if routing_method == 'Hadlocks':
-                try:
-                    Hadlocks(sb)
-                    #sb.printDemands()
-                except:
-                    #drawSB(sb)
-                    continue
-                else:
+                routed_success = Hadlocks(sb)
+                if routed_success == len(demands):
                     success_counter = success_counter + 1   
-    
+
+            if routing_method == 'RandomHadlocks':
+                routed_success = RandomHadlocks(sb)
+                if routed_success == len(demands):
+                    success_counter = success_counter + 1   
+                else:
+                    print(getAllStats(sb))
+                    drawSB(sb)
+                    break
+            
+            completeness = 100*((number_demands*iterations+i+1)/((max_demands+1)*iterations))         
+            progress(completeness)
         results.append(success_counter)
-
+    endprogress()
     for i,r in enumerate(results):
-        percentage = 100 * r/iterations
+        percentage = round(100 * r/iterations,2)
         print("For " + str(i+1) + " demands, success -> " + str(r) + "/" + str(iterations) + " (" + str(percentage) + "%)")
+    return sb
 
-def genDemand(width,routeback):
+##########################################
+# Generate a pair of two nodes to pass as a demand to be routed
+# routeback -> whether the two nodes can be on the same side
+# common_nets -> whether any two nodes in a set (prev demands passed as demands) can be the same
+def genDemand(width,routeback,common_nets,demands):
+
     done = False
     
     while done == False:
@@ -78,11 +104,19 @@ def genDemand(width,routeback):
             done = False
         if routeback == False and (d1[0] == d2[0]):
             done = False
+        if demands and common_nets==False:
+            for prev_demand in demands:
+                if (d1[0],d1[1]) in prev_demand:
+                    done = False
+                elif (d2[0],d2[1]) in prev_demand:
+                    done = False
+
     demand = ((d1[0],d1[1]),(d2[0],d2[1]))
     return(demand)
 
 
-
+#################################
+# Generate a random node
 def genDest(width):
     i=0
     i = random.randint(0,3)
@@ -95,22 +129,8 @@ def genDest(width):
         pole = 'S'
     if i==3:
         pole = 'W'
+    # Here the corner nodes (=0 and=W-1) and not included to avoid traps
     i = random.randint(1,width-2)
     
     return(pole,i)
 
-def getRouteEfficiency(sb):
-    if not sb.routes:
-        return 1
-    total_len = 0
-    total_man = 0
-    for r in sb.routes:
-        length = len(r) - 3
-        total_len = total_len + length
-        manhatten = manhat(r[1],r[-2])
-        total_man = total_man + manhatten
-        id = str(r[0]) + str(r[-1])
-        percent = 100*(manhatten/length)
-        print("For route " + id + ", length = " + str(length) + " compared to possible " + str(manhatten) + " (" + str(percent) + "%)")
-    overall_percent = 100*total_man/total_len
-    print("Overall Efficiency = " + str(overall_percent) + "%")
